bb246e0045f27c12865937b43de091718b3cf886
Daisy Oduor <Daisy.Oduor@cciea.corp>
2024-12-17 12:56:11 +0300

    Contacts page

---
+++ app/contact/page.js
@@ -0,0 +1,14 @@
+const Contact = () => {
+    return(
+
+        <>
+            <h1 className="title">Contact us</h1>
+            <p>Find us through the following platforms</p>
+        </>
+        
+
+
+    )
+}
+
+export default Contact;
