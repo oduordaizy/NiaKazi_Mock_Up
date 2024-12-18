fb277eda8925753cf9f84d650c067ed3d8ce1e20
achdaisy <oduordaizy@gmail.com>
2024-12-18 23:21:09 +0300

    few changes

--- app/recruiter/login/page.js
+++ app/recruiter/login/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { LoginForm } from "@/components/login-form"
 
 function RecriterLogin() {
   return (
-    <div>Dear recruiter, welcome to the login page</div>
+    <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
 
